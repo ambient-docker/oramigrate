7251a14b457fc1d9da87c8c78421894128d85e74
Herve <dockerlite@gmail.com>
2018-12-02 10:27:13 +0100

    Added tpc-ds benchmark tools

--- sql_tutorial/create_table.sql
+++ sql_tutorial/create_table.sql
@@ -1,4 +1,2 @@
-CREATE DATABASE tutorial
-     DATAFILE 'tutorial_datafile' SIZE 10M
-     LOGFILE GROUP 1 ('test_log1w', 'test_log1z') SIZE 500K,
-     GROUP 2 ('test_log2w', 'test_log2w') SIZE 500K;
+# affiche le nom de la base de donnees
+select * from global_name;
