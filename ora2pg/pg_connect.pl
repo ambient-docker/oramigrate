52873272a54fa84aad3cb6b85858c4181aa9ada2
Herve <dockerlite@gmail.com>
2018-12-30 05:10:59 +0100

    deleted files and setup ora2pg in Dockerfile

--- ora2pg/pg_connect.pl
+++ ora2pg/pg_connect.pl
@@ -8,6 +8,6 @@ my $host = 'postgresql-11';
 my $port    = 5432;
 my $dbname = 'template1';
 my $user = 'postgres';
-my$pass = 'password';
+my $pass = 'password';
 
 my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;port=$port", $user, $pass) ||  die "Failed to connect: $DBI::errstr\n";
